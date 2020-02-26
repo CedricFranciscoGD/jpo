2211342ed5e84a905f2e64d9bdcf61157c448679
CedricFranciscoGD <franciscomca@live.fr>
2020-02-26 19:27:45 +0100

    questions

--- jpo.c
+++ jpo.c
@@ -11,12 +11,136 @@ int main (){
 	int scoreJoueur;
 	char nomJoueur;
 
-
+	scoreJoueur=0;
 
 	printf("Veuillez inserer votre prenom:\n");
 	scanf("%s",&nomJoueur);
-	printf("Bonjour et bienvenue %s vous souhaitez vous renseignez sur le metier de game designer et plus generalement sur la formation de game design proposee par l'ETPA\nVeuillez repondre aux 10 questions qui vont suivre, un score vous sera attribue en fonction de vos reponse afin de jauger votre compatibilite avec la formation",&nomJoueur);
+	printf("Bonjour et bienvenue %s vous souhaitez vous renseignez sur le metier de game designer et plus generalement sur la formation de game design proposee par l'ETPA\nVeuillez repondre aux 10 questions qui vont suivre, un score vous sera attribue en fonction de vos reponses afin de jauger votre compatibilite avec la formation",&nomJoueur);
 	
 
+	printf("6- Si tu devais choisir un versus fighting, lequel serait-il ? \n");
+	printf("1-Street Fighter\n2-Soul Calibur\n3-Tekken\n4-Mortal Kombat\n");
+	scanf("%d",&choixRep);
+	if(choixRep==1)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==2)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("Vous avez %d points \n", scoreJoueur);
+	
+
+	printf("\n");
+	printf("7- Si tu devais choisir un jeu de strategie/gestion, lequel prendrais-tu ? \n");
+	printf("1-Age of Empire\n2-Civilization\n3-Warcraft\n4-Les Sims\n");
+	scanf("%d",&choixRep);
+	if(choixRep==2)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("Vous avez %d points \n", scoreJoueur);
+	printf("\n");
+	printf("8- Si tu devais choisir un jeu mobile, ce serait lequel ? \n");
+	printf("1-Candy Crush Saga\n2-DBZ Dokkan Battle\n3-Clash Royal\n4-Pokemon Go\n");
+	scanf("%d",&choixRep);
+	if(choixRep==1)
+	{
+		scoreJoueur-=2;
+	}
+	else if(choixRep==2)
+	{
+		scoreJoueur--;
+	}
+	else if(choixRep==3)
+	{
+		scoreJoueur--;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("Vous avez %d points \n", scoreJoueur);
+	printf("\n");
+	printf("9- Si tu devais choisir un paltformer, sur lequel sauterais-tu ? \n");
+	printf("1-Mario\n2-Rayman\n3-Sonic\n4-Alex Kidd\n");
+	scanf("%d",&choixRep);
+	if(choixRep==1)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==3)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("Vous avez %d points \n", scoreJoueur);
+	printf("\n");
+	printf("10- Si tu devais choisir un survival horror, pour lequel frissonerais-tu ? \n");
+	printf("1-Resident Evil\n2-Alone In The Dark\n3-The Evil Within\n4-Alien Isolation\n");
+	scanf("%d",&choixRep);
+	if(choixRep==1)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==2)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("Vous avez %d points \n", scoreJoueur);
+	printf("\n");
+	printf("11- Si tu devais choisir un jeu de rythme/musique, sur lequel t'ambiancerais-tu ?\n");
+	printf("1-Street Fighter\n2-Soul Calibur\n3-Tekken\n4-Mortal Kombat\n");
+	scanf("%d",&choixRep);
+	if(choixRep==2)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur--;
+	}
+	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
+	{
+		printf("ATTENTION, '%d' n'est pas compris dans les choix disponible\n",choixRep );
+		printf("Choisissez une reponse valide\n");
+		scanf("%d",&choixRep);
+	}
+	printf("\n");
+	printf("Vous avez %d points \n", scoreJoueur);
 	return 0;
 }
