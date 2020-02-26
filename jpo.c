3cc0297fa8060a10728d6cb1a5e7ff2059e68d42
CedricFranciscoGD <franciscomca@live.fr>
2020-02-26 18:33:09 +0100

    intro

---
+++ jpo.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// Bibliothèque permettant de générer des nombres aléatoire pour programmer le comportement du monstre
+#include <stdlib.h>
+#include <time.h>
+
+
+
+int main (){
+
+	int choixRep;
+	int scoreJoueur;
+	char nomJoueur;
+
+
+
+	printf("Veuillez inserer votre prenom:\n");
+	scanf("%s",&nomJoueur);
+	printf("Bonjour et bienvenue %s vous souhaitez vous renseignez sur le metier de game designer et plus generalement sur la formation de game design proposee par l'ETPA\nVeuillez repondre aux 10 questions qui vont suivre, un score vous sera attribue en fonction de vos reponse afin de jauger votre compatibilite avec la formation",&nomJoueur);
+	
+
+	return 0;
+}
