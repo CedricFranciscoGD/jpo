565f62dad5496c108ef4140dfe64b18950597115
CedricFranciscoGD <franciscomca@live.fr>
2020-02-26 20:34:28 +0100

    Update jpo.c

--- jpo.c
+++ jpo.c
@@ -24,22 +24,22 @@ int main (){
     
     if (choixRep==1){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==2){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==3){
         
-        scoreJoueur += -1;
+        scoreJoueur -= 1;
     }
     
     if (choixRep==4){
         
-        scoreJoueur += +1;
+        scoreJoueur += 2;
     }
     while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -48,6 +48,7 @@ int main (){
 		scanf("%d",&choixRep);
 	}
     printf("Score : %d.\n", scoreJoueur);
+    printf("\n");
     
     //QUESTION 2 ______________________________
     printf("Si tu devais choisir un jeu FPS, tu choisirais :\n1-Halo \n2-Call Of Duty \n3-Counter Strike GO \n4-Rainbow 6 Siege\n");
@@ -55,7 +56,7 @@ int main (){
     
     if (choixRep==1){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==2){
@@ -65,12 +66,12 @@ int main (){
     
     if (choixRep==3){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==4){
         
-        scoreJoueur += +1;
+        scoreJoueur += 2;
     }
     while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -79,9 +80,10 @@ int main (){
 		scanf("%d",&choixRep);
 	}
     printf("Score : %d.\n", scoreJoueur);
+    printf("\n");
     
     //QUESTION 3 ______________________________
-    printf("Si tu devais choisir un jeu de simulation sportive, tu choisirais :\n1-Fifa \n2-Wii Sport \n3-Just Dance \n4-Rainbow 6 NBA\n");
+    printf("Si tu devais choisir un jeu de simulation sportive, tu choisirais :\n1-Fifa \n2-Wii Sport \n3-Just Dance \n4-NBA\n");
     scanf("%d",&choixRep);
     
     if (choixRep==1){
@@ -91,17 +93,17 @@ int main (){
     
     if (choixRep==2){
         
-        scoreJoueur += +1;
+        scoreJoueur += 2;
     }
     
     if (choixRep==3){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==4){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -110,6 +112,7 @@ int main (){
 		scanf("%d",&choixRep);
 	}
     printf("Score : %d.\n", scoreJoueur);
+    printf("\n");
     
     //QUESTION 4 ______________________________
     printf("Si tu devais choisir un jeu de course, tu choisirais :\n1-Forza \n2-Need For Speed \n3-Mario Kart \n4-Gran Turismo\n");
@@ -117,22 +120,22 @@ int main (){
     
     if (choixRep==1){
         
-        scoreJoueur += +1;
+        scoreJoueur += 2;
     }
     
     if (choixRep==2){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==3){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==4){
         
-        scoreJoueur += -1;
+        scoreJoueur -= 1;
     }
     while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -141,6 +144,7 @@ int main (){
 		scanf("%d",&choixRep);
 	}
     printf("Score : %d.\n", scoreJoueur);
+    printf("\n");
     
     //QUESTION 5 ______________________________
     printf("Si tu devais choisir un jeu d'aventure, tu choisirais :\n1-Tomb Raider \n2-Zelda \n3-Uncharted GO \n4-God Of War\n");
@@ -148,22 +152,22 @@ int main (){
     
     if (choixRep==1){
         
-        scoreJoueur += -1;
+        scoreJoueur -= 1;
     }
     
     if (choixRep==2){
         
-        scoreJoueur += +1;
+        scoreJoueur += 2;
     }
     
     if (choixRep==3){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     
     if (choixRep==4){
         
-        scoreJoueur += 0;
+        scoreJoueur ++;
     }
     while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -172,17 +176,26 @@ int main (){
 		scanf("%d",&choixRep);
 	}
     printf("Score : %d.\n", scoreJoueur);
+    printf("\n");
     //QUESTION 6 ______________________________
 	printf("6- Si tu devais choisir un versus fighting, lequel serait-il ? \n");
 	printf("1-Street Fighter\n2-Soul Calibur\n3-Tekken\n4-Mortal Kombat\n");
 	scanf("%d",&choixRep);
 	if(choixRep==1)
 	{
-		scoreJoueur++;
+		scoreJoueur += 2;
 	}
 	else if(choixRep==2)
 	{
 		scoreJoueur--;
+	}
+	else if(choixRep==3)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur++;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -200,11 +213,19 @@ int main (){
 	scanf("%d",&choixRep);
 	if(choixRep==2)
 	{
-		scoreJoueur++;
+		scoreJoueur +=2;
 	}
 	else if(choixRep==4)
 	{
 		scoreJoueur--;
+	}
+	else if(choixRep==1)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==3)
+	{
+		scoreJoueur++;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -220,19 +241,19 @@ int main (){
 	scanf("%d",&choixRep);
 	if(choixRep==1)
 	{
-		scoreJoueur-=2;
+		scoreJoueur--;
 	}
 	else if(choixRep==2)
 	{
-		scoreJoueur--;
+		scoreJoueur+=0;
 	}
 	else if(choixRep==3)
 	{
-		scoreJoueur--;
+		scoreJoueur+=0;
 	}
 	else if(choixRep==4)
 	{
-		scoreJoueur--;
+		scoreJoueur+=0;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -248,11 +269,19 @@ int main (){
 	scanf("%d",&choixRep);
 	if(choixRep==1)
 	{
-		scoreJoueur++;
+		scoreJoueur +=2;
 	}
 	else if(choixRep==3)
 	{
 		scoreJoueur--;
+	}
+	else if(choixRep==2)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur++;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -268,11 +297,19 @@ int main (){
 	scanf("%d",&choixRep);
 	if(choixRep==1)
 	{
-		scoreJoueur++;
+		scoreJoueur +=2;
 	}
 	else if(choixRep==2)
 	{
 		scoreJoueur--;
+	}
+	else if(choixRep==3)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur++;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
@@ -288,11 +325,19 @@ int main (){
 	scanf("%d",&choixRep);
 	if(choixRep==2)
 	{
-		scoreJoueur++;
+		scoreJoueur +=2;
 	}
 	else if(choixRep==3)
 	{
 		scoreJoueur--;
+	}
+	else if(choixRep==1)
+	{
+		scoreJoueur++;
+	}
+	else if(choixRep==4)
+	{
+		scoreJoueur++;
 	}
 	while(choixRep!=1 && choixRep!=2 && choixRep!=3 && choixRep!=4)
 	{
